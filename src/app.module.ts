d8ccfd7cccd40684dcf59cab51b330b1bbc04471
guilherhenri <guilherhenri12@gmail.com>
2023-08-29 16:59:18 -0300

    Finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { AuthModule } from './auth/auth.module'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
